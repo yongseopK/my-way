688afa063d535817ee860c43dd609ee8f11972fe
yongseopK <kk0@kakao.com>
2024-03-07 10:33:25 +0900

    # initial commit

---
+++ src/main/java/com/healthmap/myway/MyWayApplication.java
@@ -0,0 +1,13 @@
+package com.healthmap.myway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/healthmap/myway/MyWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthmap.myway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
